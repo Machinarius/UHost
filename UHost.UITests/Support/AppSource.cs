9a76e75da503b4b00f405bac0a670d1d89c69732
Germán Valencia <machinarius92@gmail.com>
2017-07-30 21:53:50 -0500

    - Reworked the ui test app startup code for stability - Installed PhantomJS to run tests on the VSTS host

--- UHost.UITests/Support/AppSource.cs
+++ UHost.UITests/Support/AppSource.cs
@@ -1,14 +1,24 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Threading;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.PhantomJS;
 
 namespace UHost.UITests.Support {
   public static class AppSource {
     public const string AppUrl = "http://localhost:5000/";
 
     public static AppHandle GenerateWebDriver() {
-      // TODO: Launch the app here
-      // ASPNETCORE_URLS="http://localhost:5000/" ASPNETCORE_ENVIRONMENT="Testing" dotnet run
+      IWebDriver webDriver;
+
+      var inCIServer = !String.IsNullOrEmpty(Environment.GetEnvironmentVariable("TF_BUILD"));
+      if (inCIServer) {
+        webDriver = new PhantomJSDriver();
+      } else {
+        webDriver = new ChromeDriver();
+      }
 
       var appLocation = typeof(AppSource).Assembly.Location;
       appLocation = Path.Combine(appLocation, "..", "..", "..", "..", "..", "UHost");
@@ -24,10 +34,11 @@ namespace UHost.UITests.Support {
       appPSI.EnvironmentVariables.Add("ASPNETCORE_ENVIRONMENT", "Testing");
 
       var appProcess = Process.Start(appPSI);
-      var chromeDriver = new ChromeDriver();
-      chromeDriver.Navigate().GoToUrl(AppUrl);
+      Thread.Sleep(TimeSpan.FromSeconds(2));
 
-      var handle = new AppHandle(chromeDriver, appProcess);
+      webDriver.Navigate().GoToUrl(AppUrl);
+
+      var handle = new AppHandle(webDriver, appProcess);
       return handle;
     }
   }
