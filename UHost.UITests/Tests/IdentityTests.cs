d3d329f7fcb70711c0b643e0d0af5cf489fd8867
Germán Valencia <machinarius92@gmail.com>
2017-08-01 21:04:24 -0500

    The screenshot code now cleanly logs any exceptions when saving the browser screenshot fails

--- UHost.UITests/Tests/IdentityTests.cs
+++ UHost.UITests/Tests/IdentityTests.cs
@@ -6,27 +6,33 @@ using UHost.UITests.Pages;
 using UHost.UITests.Support;
 using UHost.UITests.Utility;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace UHost.UITests {
   public class IdentityTests : IDisposable {
-    B2CUser testUser;
-    AppHandle appHandle;
-    IWebDriver webDriver;
+    private B2CUser testUser;
+    private AppHandle appHandle;
+    private IWebDriver webDriver;
+    private ITestOutputHelper output;
+
+    public IdentityTests(ITestOutputHelper output) {
+      this.output = output;
+
+      appHandle = AppSource.GenerateWebDriver();
+      webDriver = appHandle.WebDriver;
+    }
 
     [Fact]
     public async Task SigningInShouldIdentifyMeAsAUserInThePlatformAsync() {
       testUser = new B2CUser("carlos@test.com", "carlosIsGreat123", "Carlos Quintero");
       await testUser.RegisterAsync();
       
-      appHandle = AppSource.GenerateWebDriver();
-      webDriver = appHandle.WebDriver;
-
       var loginPage = new LoginPage(webDriver);
       loginPage.Login(testUser);
     }
     
     public void Dispose() {
-      webDriver.DumpScreenshot();
+      webDriver.DumpScreenshot(output);
 
       testUser?.CleanupAsync().Wait();
       appHandle?.Dispose();
--- UHost.UITests/Utility/WebDriverExtensions.cs
+++ UHost.UITests/Utility/WebDriverExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using OpenQA.Selenium;
+using Xunit.Abstractions;
 
 namespace UHost.UITests.Utility {
   public static class WebDriverExtensions {
@@ -12,26 +13,31 @@ namespace UHost.UITests.Utility {
       return webDriver.FindElement(By.XPath($"//*[contains(text(), '{elementText}')]"));
     }
 
-    public static void DumpScreenshot(this IWebDriver webDriver) {
+    public static void DumpScreenshot(this IWebDriver webDriver, ITestOutputHelper output) {
       if (webDriver == null) {
         throw new ArgumentNullException(nameof(webDriver));
       }
 
-      var screenshotsFolder = "Screenshots";
-      var agentWorkFolder = Environment.GetEnvironmentVariable("AGENT_WORKFOLDER");
-      if (!string.IsNullOrEmpty(agentWorkFolder)) {
-        screenshotsFolder = Path.Combine(agentWorkFolder, screenshotsFolder);
+      if (output == null) {
+        throw new ArgumentNullException(nameof(output));
       }
 
+      var screenshotsFolder = "Screenshots";
       if (!Directory.Exists(screenshotsFolder)) {
         Directory.CreateDirectory(screenshotsFolder);
       }
 
       var takesScreen = (ITakesScreenshot)webDriver;
       var screenshot = takesScreen.GetScreenshot();
-      var screenshotPath = Path.Combine("Screenshots", 
+      var screenshotPath = Path.Combine(screenshotsFolder, 
         Path.GetFileNameWithoutExtension(Path.GetRandomFileName()) + ".png");
-      screenshot.SaveAsFile(screenshotPath, ScreenshotImageFormat.Png);
+
+      try {
+        screenshot.SaveAsFile(screenshotPath, ScreenshotImageFormat.Png);
+        output.WriteLine("Saved final screenshot: " + screenshotPath);
+      } catch (Exception) {
+        output.WriteLine("Failed to saved final screenshot: " + screenshotPath);
+      }
     }
   }
 }
